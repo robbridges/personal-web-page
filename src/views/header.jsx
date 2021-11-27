e110813aa2b81c16a16e2a04abc32bfa78325674
robbridges <robbridges@users.noreply.github.com>
2021-11-26 20:31:44 -0800

    Few small changes

--- src/views/header.jsx
+++ src/views/header.jsx
@@ -5,8 +5,8 @@ class Header extends React.Component {
     return (
         <div className= "App-header">
           <div className = "left-side">
-            <a className="header-link" href="https://google.com"> Rob Bridges</a>
-            <a className="header-link" href="google.com">Recent work</a>
+            <a className="header-link" href="https://robbridges.github.io/personal-web-page/"> Rob Bridges</a>
+            <a className="header-link" href="https://robbridges.github.io/personal-web-page/">Recent work</a>
           </div>
         </div>
         
