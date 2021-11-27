674088cbc54c4d0d2feeb2e1947a15f7146a49f2
robbridges <robbridges@users.noreply.github.com>
2021-11-26 20:30:24 -0800

    Working on links, don't lead anywhere yet

--- src/views/header.jsx
+++ src/views/header.jsx
@@ -4,7 +4,10 @@ class Header extends React.Component {
   render() {
     return (
         <div className= "App-header">
-          <p className ="Header-content"></p>
+          <div className = "left-side">
+            <a className="header-link" href="https://google.com"> Rob Bridges</a>
+            <a className="header-link" href="google.com">Recent work</a>
+          </div>
         </div>
         
       
