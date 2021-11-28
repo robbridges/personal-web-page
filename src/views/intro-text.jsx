4410e07a09c002663723ffce0ba21dd2e8475712
robbridges <robbridges@users.noreply.github.com>
2021-11-27 20:11:37 -0800

    Making it look the same across different screen sizes, buttons still need work

--- src/views/intro-text.jsx
+++ src/views/intro-text.jsx
@@ -7,6 +7,7 @@ class IntroText extends React.Component {
         <h1 className="primary-heading">
           <span className="introduction-main">Robert Bridges</span>
           <span className="introduction-sub">Full Stack Developer</span>
+          
         </h1>
         <div className ="intro-links">
           <span className="intro-button"><a className="btn github-button button-animation" target="_blank" rel="noreferrer" href="https://github.com/robbridges">Github</a></span>
