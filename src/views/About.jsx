6fca5cc21e22c4b1793811f0b33c64615570fcc1
robbridges <robbridges@users.noreply.github.com>
2021-11-30 16:17:43 -0800

    About me page basics up

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -6,8 +6,15 @@ class About extends React.Component {
     return (
       <div className="AboutMeText">
         <div className="AboutMeHeading">
-          <h2 className="AboutMeHeader"> About me</h2>
-          <p>... Coming soon</p>
+          <h2 className="AboutMeHeader"> About me:</h2>
+          <p className="AboutMeParagraph"> My name is Rob Bridges, I am a recent graduate of a online full-stack javascript bootcamp where I was certified in the basics of React, Node, MySQL, and Express.JS
+          Though after that I have expanded my skills to work with: MongoDB, Docker, Kubernetes. As strange as it may seem to say, I am absolutely in love with the constant learning environment that coding forces you to have <br>
+
+          </br>
+          <br>
+          </br>
+          I have a full time career as a Ecommerce Fraud Anaylst but am looking for a career change into a full stack javascript position, if you would like to reach out to me, feel free to reach me at Rmb101187@gmail.com
+          </p>
         </div>
         
       </div>
