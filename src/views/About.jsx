ec27e9a6715d981c2a3666c50283cec5cac7d381
robbridges <robbridges@users.noreply.github.com>
2021-12-01 14:09:56 -0800

    More small adjustments

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -8,8 +8,9 @@ class About extends React.Component {
         <div className="AboutMeHeading">
           <h2 className="AboutMeHeader"> About me:</h2>
           <p className="AboutMeParagraph"> My name is Rob Bridges, I am a recent graduate of a online full-stack javascript bootcamp where I was certified in the basics of React, 
-          Node, MySQL, and Express.JS I am currently looking for a career change into Full Stack Javascript Engineering. I am a constant learner, and adament problem solver. While I definiately 
+          Node, MySQL, and Express.JS, after the bootcamp I've worked to expand my skills to include MongoDB, Docker, Kubernetes. I am currently looking for a career change into Full Stack Javascript Engineering. I am a constant learner, and adament problem solver. While I definiately 
           consider myself resourceful, I also know when I am beat and am not afraid to seek out additional help.
+          <br></br>
           <br></br>
           I can be reached at the email address Rmb101187@gmail.com if you'd like to connect.
           </p>
