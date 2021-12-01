7434559d38ecde253e05b652794dbf2568fd394b
robbridges <robbridges@users.noreply.github.com>
2021-12-01 14:08:15 -0800

    Fixing about me component

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -7,14 +7,13 @@ class About extends React.Component {
       <div className="AboutMeText">
         <div className="AboutMeHeading">
           <h2 className="AboutMeHeader"> About me:</h2>
-          <p className="AboutMeParagraph"> My name is Rob Bridges, I am a recent graduate of a online full-stack javascript bootcamp where I was certified in the basics of React, Node, MySQL, and Express.JS
-          Though after that I have expanded my skills to work with: MongoDB, Docker, Kubernetes. As strange as it may seem to say, I am absolutely in love with the constant learning environment that coding forces you to have <br>
-
-          </br>
-          <br>
-          </br>
-          I have a full time career as a Ecommerce Fraud Anaylst but am looking for a career change into a full stack javascript position, if you would like to reach out to me, feel free to reach me at Rmb101187@gmail.com
+          <p className="AboutMeParagraph"> My name is Rob Bridges, I am a recent graduate of a online full-stack javascript bootcamp where I was certified in the basics of React, 
+          Node, MySQL, and Express.JS I am currently looking for a career change into Full Stack Javascript Engineering. I am a constant learner, and adament problem solver. While I definiately 
+          consider myself resourceful, I also know when I am beat and am not afraid to seek out additional help.
+          <br></br>
+          I can be reached at the email address Rmb101187@gmail.com if you'd like to connect.
           </p>
+          
         </div>
         
       </div>
